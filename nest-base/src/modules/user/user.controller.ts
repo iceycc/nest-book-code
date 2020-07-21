a607e82631cce987a234fedaa163f5add5e1c828
kuangshp <kuangshp@126.com>
2020-07-21 12:12:05 +0800

    前端模板

--- nest-base/src/modules/user/user.controller.ts
+++ nest-base/src/modules/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render, Post, Body, Response } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+  @Get()
+  index(): string {
+    return '用户主页';
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  @Get('login')
+  @Render('login')
+  loginPage() {
+    return { title: '登录页面' };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  @Post('login')
+  login(
+    @Body() body: any,
+    @Response() res: any
+  ) {
+    console.log(body, '接受页面传递来的数据');
+    res.redirect('/user');
+  }
+}
