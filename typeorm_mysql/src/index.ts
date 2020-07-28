91ccdbdd411cbdd1ed0e160164a4202cf272512a
kuangshp <kuangshp@126.com>
2020-07-28 18:01:15 +0800

    typeorm的基本配置

--- typeorm_mysql/src/index.ts
+++ typeorm_mysql/src/index.ts
@@ -1,8 +1,21 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
 
-createConnection().then(async connection => {
+createConnection({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'typeorm_mysql',
+    synchronize: true,
+    logging: true,
+    dropSchema: true,
+    entities: [
+        'src/entity/**/*.ts'
+    ]
+}).then(async connection => {
 
     console.log("Inserting a new user into the database...");
     const user = new User();
