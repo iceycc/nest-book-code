3d68a56ce38f6dfcad71cd22edd73013b4846dc6
kuangshp <kuangshp@126.com>
2020-07-29 14:02:10 +0800

    表结构

--- typeorm_mysql/src/entity/User.ts
+++ typeorm_mysql/src/entity/User.ts
@@ -1,18 +1,55 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-@Entity()
+@Entity({ name: 'user' })
 export class User {
-
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn({
+        type: 'int',
+        name: 'id',
+        comment: '主键id'
+    })
     id: number;
 
-    @Column()
-    firstName: string;
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 50,
+        unique: true,
+        name: 'username', // 如果是一样的可以不指定
+        comment: '用户名'
+    })
+    username: string;
 
-    @Column()
-    lastName: string;
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 100,
+        comment: '密码'
+    })
+    password: string;
 
-    @Column()
-    age: number;
+    @Column('tinyint', {
+        nullable: false,
+        default: () => 0,
+        name: 'is_del',
+        comment: '是否删除,1表示删除,0表示正常'
+    })
+    isDel: number;
 
+    @CreateDateColumn({
+        type: 'timestamp',
+        nullable: false,
+        name: 'created_at', // mysql数据库规范是使用下划线命名的,不使用驼峰
+        comment: '创建时间'
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp',
+        nullable: false,
+        name: 'updated_at',
+        comment: '更新时间',
+    })
+    updateAt: Date;
 }
+
+
